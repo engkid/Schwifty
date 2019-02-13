7c900c5ae3a1cf7b8953502ee52796c1314a2211
Engkit Satia Riswara <engkidsr@gmail.com>
2019-02-13 16:17:45 +0700

    Added some comments to trace an issue

--- Schwifty/Controllers/CinemaHomeViewController/CinemaHomePresenter.swift
+++ Schwifty/Controllers/CinemaHomeViewController/CinemaHomePresenter.swift
@@ -10,6 +10,12 @@ import Foundation
 
 class CinemaHomePresenter: ICinemaHomePresenter {
     
+    /*MARK:
+     
+     this is interesting, I deallocate the presenter with 3 weak properties (view, interactor, wireframe) to move it to the homeViewController (view)... somehow got
+     nil class for interactor and wireframe fixed it by changing weak to strong reference *still have no idea whats wrong... need to figure out
+     
+     */
     var interactor: ICinemaHomeInteractor?
     weak var view: ICinemaHomeView?
     var wireframe: ICinemaHomeWireframe?

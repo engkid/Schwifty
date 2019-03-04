f62f8b6ff51b29d9421fdfdc44b420afde602320
Engkit Satia Riswara <engkidsr@gmail.com>
2019-03-04 16:10:13 +0700

    refactor detailviewcontroller

--- Schwifty/Controllers/DetailViewController/DetailViewController.swift
+++ Schwifty/Controllers/DetailViewController/DetailViewController.swift
@@ -8,47 +8,14 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, IDetailView, UIScrollViewDelegate {
+class DetailViewController: UIViewController, IDetailView {
 
     // MARK: - Properties
     var presenter: DetailPresenter?
     
-    var imageView: UIImageView = {
-        
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.layer.cornerRadius = 10
-        
-        return iv
-    }()
-    
-    var descriptionLabel: UILabel = {
-        
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.numberOfLines = 0
-        label.textColor = .white
-        
-        return label
-    }()
-    
-    lazy var scrollView: UIScrollView = {
-        
-        let scroll = UIScrollView()
-        scroll.showsHorizontalScrollIndicator = false
-        
-        scrollViewDidScroll(scroll)
-        
-        scroll.addSubview(descriptionLabel)
-        scroll.addSubview(imageView)
-        
-        imageView.anchor(top: scroll.topAnchor, left: scroll.leftAnchor, bottom: scroll.bottomAnchor, right: descriptionLabel.leftAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 100, height: 150)
-        descriptionLabel.anchor(top: scroll.topAnchor, left: imageView.rightAnchor, bottom: nil, right: scroll.rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 200, height: 0)
-        
-        scroll.backgroundColor = .gray
-       
-        return scroll
-    }()
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var avatarImage: UIImageView!
     
     // MARK: - Init
     override func viewDidLoad() {
@@ -60,25 +27,13 @@ class DetailViewController: UIViewController, IDetailView, UIScrollViewDelegate 
     // MARK: - Helper components
     func setupView(withFamily family: Families) {
         
-        let topBarHeight = UIApplication.shared.statusBarFrame.size.height +
-            (self.navigationController?.navigationBar.frame.height ?? 0.0)
-        
-        scrollView.delegate = self
-        
-        view.addSubview(scrollView)
-        scrollView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: topBarHeight, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: view.frame.width, height: view.frame.height)
-        
         guard let avatarUrl = family.avatarUrl else { return }
         
-        imageView.setImageWithUrl(url: avatarUrl)
+        title = family.name
+        statusLabel.text = family.status
+        avatarImage.setImageWithUrl(url: avatarUrl)
         descriptionLabel.text = family.description
         
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.x != 0 {
-            scrollView.contentOffset.x = 0
-        }
     }
 
 }

9b402d1e879d67c33154f149b9b37acb77100eb2
Engkit Satia Riswara <engkidsr@gmail.com>
2019-03-04 09:44:38 +0700

    Added detailViewController and contraints helper categories

---
+++ Schwifty/Controllers/DetailViewController/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Schwifty
+//
+//  Created by Engkit Satia Riswara on 04/03/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Schwifty/Controllers/DetailViewController/DetailWireframe.swift
@@ -0,0 +1,9 @@
+//
+//  DetailWireframe.swift
+//  Schwifty
+//
+//  Created by Engkit Satia Riswara on 04/03/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import Foundation

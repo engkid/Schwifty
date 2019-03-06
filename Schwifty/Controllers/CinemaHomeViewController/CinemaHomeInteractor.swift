9986e39aa2ab2347d71782947baff610bbb351b2
Engkit Satia Riswara <engkidsr@gmail.com>
2019-03-06 10:06:50 +0700

    remove hardcoded models

--- Schwifty/Controllers/CinemaHomeViewController/CinemaHomeInteractor.swift
+++ Schwifty/Controllers/CinemaHomeViewController/CinemaHomeInteractor.swift
@@ -18,15 +18,6 @@ class CinemaHomeInteractor: ICinemaHomeInteractor {
         
         var families = [Families]()
         
-//        families = [
-//            
-//            Families(id: 1, name: "Engkit", status: "Papa", avatarUrl: "https://i.imgur.com/dbucqYG.jpg", latitude: -6.9383029, longitude: 107.6663636),
-//            Families(id: 2, name: "Windy", status: "Mama", avatarUrl: "https://i.imgur.com/hdtTObG.jpg", latitude: -6.9383029, longitude: 107.6663636),
-//            Families(id: 3, name: "Windy & Engkit Jr", status: "Junior", avatarUrl: "https://i.imgur.com/ts5wsXj.jpg", latitude: -6.9383029, longitude: 107.6663636),
-//            Families(id: 4, name: "", status: "", avatarUrl: "", latitude: 0.0, longitude: 0.0)
-//            
-//        ]
-        
         guard let url = URL(string:"https://www.mocky.io/v2/5c7e20003100007b003760f5") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in

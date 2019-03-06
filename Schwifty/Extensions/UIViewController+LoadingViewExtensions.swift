1fa42bea9c42add0673b4340f90f975fd9d352f9
Engkit Satia Riswara <engkidsr@gmail.com>
2019-03-06 09:07:58 +0700

    cherry pick activity indicator contraint fix

--- Schwifty/Extensions/UIViewController+LoadingViewExtensions.swift
+++ Schwifty/Extensions/UIViewController+LoadingViewExtensions.swift
@@ -27,11 +27,12 @@ extension UIViewController {
         case .indicator:
             let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
             activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
-            activityIndicator.center.x = (self.view.frame.width / 2) - activityIndicator.bounds.width + 12
-            activityIndicator.center.y = self.view.frame.height / 2
+            view.addSubview(activityIndicator)
+            activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
             activityIndicator.hidesWhenStopped = true
             activityIndicator.style = UIActivityIndicatorView.Style.gray
-            self.view.addSubview(activityIndicator)
             activityIndicator.startAnimating()
             return activityIndicator
         case .message:
